a91b9b34330ef46cf1da8e49aece3dfd8900e03b
ch <mkmsacc@outlook.com>
2021-07-25 09:51:35 +0700

    Add fluxTest

---
+++ learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/FluxAndMonoTest.java
@@ -0,0 +1,21 @@
+package com.example.demo.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+public class FluxAndMonoTest {
+
+    @Test
+    public void fluxTest() {
+        Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
+//                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .concatWith(Flux.just("After Error"))
+                .log();
+
+        stringFlux.subscribe(System.out::println
+                , throwable -> System.err.println("Exception is " + throwable)
+                , () -> System.out.println("completed"));
+
+    }
+
+}
