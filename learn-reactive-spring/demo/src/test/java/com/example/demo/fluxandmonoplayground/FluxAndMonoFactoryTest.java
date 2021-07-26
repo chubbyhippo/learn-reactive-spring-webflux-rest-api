a88a5fd4f3b46251fc5781ce7052e5798cceae44
ch <mkmsacc@outlook.com>
2021-07-26 14:14:33 +0700

    Add monoUsingJustOrEmpty

--- learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/FluxAndMonoFactoryTest.java
+++ learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/FluxAndMonoFactoryTest.java
@@ -2,10 +2,12 @@ package com.example.demo.fluxandmonoplayground;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class FluxAndMonoFactoryTest {
     List<String> animals = Arrays.asList("dog", "cat", "bird");
@@ -35,6 +37,13 @@ public class FluxAndMonoFactoryTest {
 
         StepVerifier.create(stringFlux)
                 .expectNext("dog", "cat", "bird")
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoUsingJustOrEmpty() {
+        Mono<String> stringMono = Mono.justOrEmpty(Optional.empty());
+        StepVerifier.create(stringMono)
                 .verifyComplete();
     }
 }
