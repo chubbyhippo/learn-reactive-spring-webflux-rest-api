b9f7393cfaf736469e1e67743d33ed9d0124da46
ch <mkmsacc@outlook.com>
2021-08-03 09:45:18 +0700

    Add filterTest

---
+++ learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/FluxAndMonoFilterTest.java
@@ -0,0 +1,23 @@
+package com.example.demo.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FluxAndMonoFilterTest {
+    List<String> animals = Arrays.asList("dog", "cat", "bird");
+
+    @Test
+    public void filterTest() {
+        Flux<String> stringFlux = Flux.fromIterable(animals)
+                .filter(s -> s.startsWith("c"))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("cat")
+                .verifyComplete();
+    }
+}
