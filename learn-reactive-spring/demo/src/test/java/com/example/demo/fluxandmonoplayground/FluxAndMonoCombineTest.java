391896cfce9dd4bd791f36f8bf25fba378958dd1
ch <mkmsacc@outlook.com>
2021-08-06 18:36:27 +0700

    Add combineUsingMergeWithDelayTest

--- learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/FluxAndMonoCombineTest.java
+++ learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/FluxAndMonoCombineTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
+
 public class FluxAndMonoCombineTest {
 
     @Test
@@ -16,6 +18,22 @@ public class FluxAndMonoCombineTest {
         StepVerifier.create(mergedFlux.log())
                 .expectSubscription()
                 .expectNext("a", "b", "c", "d", "e", "f")
+                .verifyComplete();
+
+
+    }
+
+    @Test
+    public void combineUsingMergeWithDelayTest() {
+        Flux<String> firstFlux = Flux.just("a", "b", "c").delayElements(Duration.ofSeconds(1));
+        Flux<String> secondFlux = Flux.just("d", "e", "f").delayElements(Duration.ofSeconds(1));
+
+        Flux<String> mergedFlux = Flux.merge(firstFlux, secondFlux);
+
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+//                .expectNext("a", "b", "c", "d", "e", "f")
+                .expectNextCount(6)
                 .verifyComplete();
 
 
