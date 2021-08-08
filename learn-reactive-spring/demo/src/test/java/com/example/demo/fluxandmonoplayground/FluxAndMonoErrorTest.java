dd9699274e844f00fb26666d6540f450b6216ab7
ch <mkmsacc@outlook.com>
2021-08-08 09:06:06 +0700

    Add fluxErrorHandlingTest

---
+++ learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/FluxAndMonoErrorTest.java
@@ -0,0 +1,31 @@
+package com.example.demo.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class FluxAndMonoErrorTest {
+
+    @Test
+    public void fluxErrorHandlingTest() {
+
+        Flux<String> stringFlux = Flux.just("a", "b", "c")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .concatWith(Flux.just("d"))
+                .onErrorResume(throwable -> {
+                    System.out.println("Exception is : " + throwable);
+                    return Flux.just("default", "default1");
+                });
+
+
+        StepVerifier.create(stringFlux.log())
+                .expectSubscription()
+                .expectNext("a", "b", "c")
+//                .expectError(RuntimeException.class)
+//                .verify();
+                .expectNext("default", "default1")
+                .verifyComplete();
+
+
+    }
+}
