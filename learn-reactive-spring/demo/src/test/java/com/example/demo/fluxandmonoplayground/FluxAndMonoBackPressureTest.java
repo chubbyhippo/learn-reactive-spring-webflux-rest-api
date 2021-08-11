c04ae5d5ec6f7e1ae5c8b6d9b48823ccdfd4a709
ch <mkmsacc@outlook.com>
2021-08-11 21:30:28 +0700

    Add backPressureTest

---
+++ learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/FluxAndMonoBackPressureTest.java
@@ -0,0 +1,22 @@
+package com.example.demo.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class FluxAndMonoBackPressureTest {
+
+    @Test
+    public void backPressureTest() {
+        Flux<Integer> range = Flux.range(1, 10);
+
+        StepVerifier.create(range)
+                .expectSubscription()
+                .thenRequest(1)
+                .expectNext(1)
+                .thenRequest(2)
+                .expectNext(2, 3)
+                .thenCancel()
+                .verify();
+    }
+}
