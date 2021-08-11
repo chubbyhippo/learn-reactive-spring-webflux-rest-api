9585ac7cf20cbcd2e6c6a9dbb954b0e93adef823
ch <mkmsacc@outlook.com>
2021-08-11 21:13:01 +0700

    Add infiniteSequenceTest

--- learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/FluxAndMonoWithTimeTest.java
+++ learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/FluxAndMonoWithTimeTest.java
@@ -2,6 +2,7 @@ package com.example.demo.fluxandmonoplayground;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 import java.time.Duration;
 
@@ -14,5 +15,19 @@ public class FluxAndMonoWithTimeTest {
         interval.subscribe(aLong -> System.out.println("Value is : " + aLong));
 
         Thread.sleep(3000);
+    }
+
+    @Test
+    public void infiniteSequenceTest() {
+
+        Flux<Long> interval = Flux.interval(Duration.ofMillis(200))
+                .take(3)
+                .log();
+
+        StepVerifier.create(interval)
+                .expectSubscription()
+                .expectNext(0L, 1L ,2L)
+                .verifyComplete();
+
     }
 }
