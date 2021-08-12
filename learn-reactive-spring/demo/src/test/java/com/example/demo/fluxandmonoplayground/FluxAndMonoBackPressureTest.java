1999e1a185b4a25fab71ecf1c081fd340cf380d8
ch <mkmsacc@outlook.com>
2021-08-12 18:45:54 +0700

    Add backPressure

--- learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/FluxAndMonoBackPressureTest.java
+++ learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/FluxAndMonoBackPressureTest.java
@@ -1,6 +1,10 @@
 package com.example.demo.fluxandmonoplayground;
 
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -18,5 +22,41 @@ public class FluxAndMonoBackPressureTest {
                 .expectNext(2, 3)
                 .thenCancel()
                 .verify();
+    }
+
+    @Test
+    public void backPressure() {
+        Flux<Integer> integerFlux = Flux.range(1, 10)
+                .log();
+// deprecated
+//        integerFlux.subscribe(integer -> System.out.println("Element is : " + integer)
+//                , throwable -> System.out.println("Exception is : " + throwable)
+//                , () -> System.out.println("Done")
+//                , subscription -> subscription.request(2));
+
+        integerFlux.subscribe(new Subscriber<>() {
+
+            @Override
+            public void onSubscribe(Subscription s) {
+                s.request(2);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println("Element is : " + integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println("Exception is : " + t);
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("Done");
+            }
+        });
+
+
     }
 }
