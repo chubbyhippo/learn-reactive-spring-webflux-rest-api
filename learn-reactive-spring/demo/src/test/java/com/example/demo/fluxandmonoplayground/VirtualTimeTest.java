57396d7e5adc7ede1ee39e18415b25820e2c29b4
ch <mkmsacc@outlook.com>
2021-08-13 21:29:15 +0700

    Add withVirtualTimeTest

--- learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/VirtualTimeTest.java
+++ learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/VirtualTimeTest.java
@@ -3,6 +3,7 @@ package com.example.demo.fluxandmonoplayground;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
 
 import java.time.Duration;
 
@@ -15,5 +16,20 @@ public class VirtualTimeTest {
                 .expectSubscription()
                 .expectNext(0L, 1L, 2L)
                 .verifyComplete();
+    }
+
+    @Test
+    public void withVirtualTimeTest() {
+        VirtualTimeScheduler.getOrSet();
+
+        Flux<Long> longFlux = Flux.interval(Duration.ofSeconds(1)).take(3).log();
+
+        StepVerifier.withVirtualTime(() -> longFlux)
+                .expectSubscription()
+                .thenAwait(Duration.ofSeconds(3))
+                .expectNext(0L, 1L, 2L)
+                .verifyComplete();
+
+
     }
 }
