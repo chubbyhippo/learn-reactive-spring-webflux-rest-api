e96832a1175c73d9e0954e1e55566867e16cb296
ch <mkmsacc@outlook.com>
2021-08-13 21:10:54 +0700

    Add hotPublisher

--- learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/ColdAndHotPublisherTest.java
+++ learn-reactive-spring/demo/src/test/java/com/example/demo/fluxandmonoplayground/ColdAndHotPublisherTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.fluxandmonoplayground;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
@@ -13,6 +14,20 @@ public class ColdAndHotPublisherTest {
         integerFlux.subscribe(integer -> System.out.println("Subscriber 1 : " + integer));
         Thread.sleep(2000);
         integerFlux.subscribe(integer -> System.out.println("Subscriber 2 : " + integer));
+        Thread.sleep(4000);
+    }
+
+    @Test
+    public void hotPublisher() throws InterruptedException {
+
+        Flux<Integer> integerFlux = Flux.just(1, 2, 3, 4, 5, 6, 7).delayElements(Duration.ofSeconds(1));
+
+        ConnectableFlux<Integer> connectableFlux = integerFlux.publish();
+        connectableFlux.connect();
+        connectableFlux.subscribe(integer -> System.out.println("Subscriber 1 : " + integer));
+        Thread.sleep(3000);
+
+        connectableFlux.subscribe(integer -> System.out.println("Subscriber 2 : " + integer));
         Thread.sleep(4000);
     }
 }
