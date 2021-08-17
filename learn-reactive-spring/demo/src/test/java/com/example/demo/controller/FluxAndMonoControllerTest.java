cf480c444f7464612053817a76fe855d3cbca2e3
ch <mkmsacc@outlook.com>
2021-08-17 16:51:05 +0700

    Add fluxEntityExchangeTEst

--- learn-reactive-spring/demo/src/test/java/com/example/demo/controller/FluxAndMonoControllerTest.java
+++ learn-reactive-spring/demo/src/test/java/com/example/demo/controller/FluxAndMonoControllerTest.java
@@ -1,12 +1,17 @@
 package com.example.demo.controller;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -44,6 +49,22 @@ class FluxAndMonoControllerTest {
                 .isOk()
                 .expectBodyList(Integer.class)
                 .hasSize(4);
+    }
+
+    @Test
+    public void fluxEntityExchangeTEst() {
+
+        List<Integer> expectedIntegerList = Arrays.asList(1, 2, 3, 4);
+
+        EntityExchangeResult<List<Integer>> listEntityExchangeResult = webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(Integer.class)
+                .returnResult();
+
+        Assertions.assertThat(expectedIntegerList).isEqualTo(listEntityExchangeResult.getResponseBody());
     }
 
 }
