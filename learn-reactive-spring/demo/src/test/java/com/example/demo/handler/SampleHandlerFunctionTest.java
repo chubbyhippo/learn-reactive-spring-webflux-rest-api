ddef53b44d4fcf109bc4fd87c3d791dad191deb1
ch <mkmsacc@outlook.com>
2021-08-22 17:28:22 +0700

    Add functional flux sample handler function test

---
+++ learn-reactive-spring/demo/src/test/java/com/example/demo/handler/SampleHandlerFunctionTest.java
@@ -0,0 +1,39 @@
+package com.example.demo.handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class SampleHandlerFunctionTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void fluxTest() {
+        Flux<Integer> responseBody = webTestClient.get().uri("/functional/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(responseBody)
+                .expectSubscription()
+                .expectNext(1)
+                .expectNext(2, 3, 4)
+                .verifyComplete();
+    }
+
+}
