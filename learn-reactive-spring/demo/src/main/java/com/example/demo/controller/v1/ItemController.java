0801418e60328a43347f178fa301ce4b1fa0b0ba
ch <mkmsacc@outlook.com>
2021-08-29 20:35:20 +0700

    Create ItemController

---
+++ learn-reactive-spring/demo/src/main/java/com/example/demo/controller/v1/ItemController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller.v1;
+
+import com.example.demo.document.Item;
+import com.example.demo.repository.ItemReactiveRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import static com.example.demo.constants.ItemConstants.ITEM_END_POINT_V1;
+
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class ItemController {
+    private ItemReactiveRepository itemReactiveRepository;
+
+    @GetMapping(ITEM_END_POINT_V1)
+    public Flux<Item> getAllItems() {
+        return itemReactiveRepository.findAll();
+    }
+
+
+}
