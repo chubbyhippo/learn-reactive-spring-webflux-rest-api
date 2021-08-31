302ac95cb4c30e8d1eee7a02856dc6130c6d8b3e
ch <mkmsacc@outlook.com>
2021-08-31 20:31:48 +0700

    Add ItemControllerTest

---
+++ learn-reactive-spring/demo/src/test/java/com/example/demo/controller/v1/ItemControllerTest.java
@@ -0,0 +1,58 @@
+package com.example.demo.controller.v1;
+
+import com.example.demo.constants.ItemConstants;
+import com.example.demo.document.Item;
+import com.example.demo.repository.ItemReactiveRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+@DirtiesContext
+@ActiveProfiles("test")
+class ItemControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    public List<Item> data() {
+        return Arrays.asList(new Item(null, "Samsung Monitor", 80000.0),
+                new Item(null, "LG Monitor", 420.0),
+                new Item(null, "Acer Monitor", 1420.99),
+                new Item("abc", "Dell Monitor", 1500.0));
+    }
+
+    @BeforeEach
+    public void setUp() {
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(data()))
+                .flatMap(itemReactiveRepository::save)
+                .doOnNext(item -> System.out.println("Inserted item is : " + item))
+                .blockLast();
+    }
+
+    @Test
+    public void getAllItems() {
+        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Item.class)
+                .hasSize(4);
+    }
+
+}
