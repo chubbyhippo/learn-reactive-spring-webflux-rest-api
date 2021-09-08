0f801de780b359a54f9d799d5ce44ec56ff63a2b
ch <mkmsacc@outlook.com>
2021-09-08 16:19:21 +0700

    Add oneItem endpoint

--- learn-reactive-spring/demo/src/main/java/com/example/demo/controller/v1/ItemController.java
+++ learn-reactive-spring/demo/src/main/java/com/example/demo/controller/v1/ItemController.java
@@ -4,9 +4,13 @@ import com.example.demo.document.Item;
 import com.example.demo.repository.ItemReactiveRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import static com.example.demo.constants.ItemConstants.ITEM_END_POINT_V1;
 
@@ -22,5 +26,10 @@ public class ItemController {
         return itemReactiveRepository.findAll();
     }
 
-
+    @GetMapping(ITEM_END_POINT_V1 + "/{id}")
+    public Mono<ResponseEntity<Item>> getOneItem(@PathVariable String id) {
+        return itemReactiveRepository.findById(id)
+                .map(item -> new ResponseEntity<>(item, HttpStatus.OK))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 }
