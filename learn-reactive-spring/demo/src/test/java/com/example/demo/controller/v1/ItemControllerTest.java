a43902e0e18a85d571a07ee06310bbbbbf37c095
ch <mkmsacc@outlook.com>
2021-09-11 17:14:21 +0700

    Add delete item test

--- learn-reactive-spring/demo/src/test/java/com/example/demo/controller/v1/ItemControllerTest.java
+++ learn-reactive-spring/demo/src/test/java/com/example/demo/controller/v1/ItemControllerTest.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller.v1;
 
-import com.example.demo.constants.ItemConstants;
 import com.example.demo.document.Item;
 import com.example.demo.repository.ItemReactiveRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,6 +18,7 @@ import reactor.test.StepVerifier;
 import java.util.Arrays;
 import java.util.List;
 
+import static com.example.demo.constants.ItemConstants.ITEM_END_POINT_V1;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -51,7 +51,7 @@ class ItemControllerTest {
 
     @Test
     public void getAllItemsHasSizeTest() {
-        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+        webTestClient.get().uri(ITEM_END_POINT_V1)
                 .exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
@@ -61,7 +61,7 @@ class ItemControllerTest {
 
     @Test
     public void getAllItemsHasSizeConsumeWithTest() {
-        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+        webTestClient.get().uri(ITEM_END_POINT_V1)
                 .exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
@@ -75,7 +75,7 @@ class ItemControllerTest {
 
     @Test
     public void getAllItemsHasSizeWithStepVerifierTest() {
-        Flux<Item> responseBody = webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+        Flux<Item> responseBody = webTestClient.get().uri(ITEM_END_POINT_V1)
                 .exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
@@ -90,7 +90,7 @@ class ItemControllerTest {
 
     @Test
     public void getOneItemTest() {
-        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"), "abc")
+        webTestClient.get().uri(ITEM_END_POINT_V1.concat("/{id}"), "abc")
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody()
@@ -99,7 +99,7 @@ class ItemControllerTest {
 
     @Test
     public void getOneItemNotFoundTest() {
-        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"), "abcefg")
+        webTestClient.get().uri(ITEM_END_POINT_V1.concat("/{id}"), "abcefg")
                 .exchange()
                 .expectStatus().isNotFound();
     }
@@ -107,7 +107,7 @@ class ItemControllerTest {
     @Test
     public void createItemTest() {
         Item item = new Item(null, "Ipad", 1000.99);
-        webTestClient.post().uri(ItemConstants.ITEM_END_POINT_V1)
+        webTestClient.post().uri(ITEM_END_POINT_V1)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(item), Item.class)
                 .exchange()
@@ -116,6 +116,20 @@ class ItemControllerTest {
                 .jsonPath("$.id").isNotEmpty()
                 .jsonPath("$.description").isEqualTo("Ipad")
                 .jsonPath("$.price").isEqualTo(1000.99);
+
+    }
+
+    @Test
+    public void deleteItemTest() {
+        webTestClient.delete().uri(ITEM_END_POINT_V1.concat("/{id}"), "abc")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Void.class);
+
+        StepVerifier.create(itemReactiveRepository.findAll())
+                .expectNextCount(3)
+                .verifyComplete();
 
     }
 
